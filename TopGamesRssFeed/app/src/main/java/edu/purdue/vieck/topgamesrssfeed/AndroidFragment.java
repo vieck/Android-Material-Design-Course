97beb7c741b84e77353e4fcfabd5289e39ea7ae7
vieck <vieck@purdue.edu>
2015-07-05 01:46:30 -0400

    Defaulted TopGamesRssFeed

--- TopGamesRssFeed/app/src/main/java/edu/purdue/vieck/topgamesrssfeed/AndroidFragment.java
+++ TopGamesRssFeed/app/src/main/java/edu/purdue/vieck/topgamesrssfeed/AndroidFragment.java
@@ -1,20 +1,23 @@
 package edu.purdue.vieck.topgamesrssfeed;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by vieck on 7/3/15.
  */
 public class AndroidFragment extends Fragment {
+    TextView mTitle;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.android_rss_feed, container, false);
+        mTitle = (TextView) view.findViewById(R.id.android_title);
         return view;
     }
 }
